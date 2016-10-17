2db1491fcaa5d9661f0040802fa7ca48ab6a88f1
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-10-17 18:32:51 +0900

    queue

--- dataStructure_CS/Program.cs
+++ dataStructure_CS/Program.cs
@@ -10,27 +10,27 @@ namespace ConsoleApplication3
     class Program
     {
         int N = 5;
-        int[] Q = new int[N];
+        int[] Q = new int[5];
         int head = 0, tail = 0;
-        
+
         static void Main(string[] args)
         {
-            int choice;
+            string choice;
             Console.WriteLine("1.삽입\n2.삭제\n3.printout\n4.the end\n");
             Console.Write("숫자를 입력하시요: ");
-            choice = Console.Read();
+            choice = Console.ReadLine();
 
             switch (choice) {
-                case 1:
+                case "1":
                     enqueue();
                     break;
-                case 2:
+                case "2":
                     dequeue();
                     break;
-                case 3:
+                case "3":
                     printData();
                     break;
-                case 4:
+                case "4":
                     Console.WriteLine("the end");
                     //exit(0);
                     break;
@@ -40,7 +40,7 @@ namespace ConsoleApplication3
 
             }
         }
-        int enqueue(){
+        public int enqueue(){
             if ((tail+ 1) % N == head) {
                 Console.WriteLine("errer; overflow.");
                 return 0;
@@ -48,10 +48,10 @@ namespace ConsoleApplication3
             Console.Write("삽입할 숫자를 입력하세요: ");
 
             tail = (tail + 1) % N;
-            Q[tail] = Console.read();
+            Q[tail] = Console.Read();
             return 1;
         }
-        int dequeue() {
+        public int dequeue() {
             if (head == tail) {
                 Console.WriteLine("errer; underflow.");
                 return 0;
@@ -60,15 +60,17 @@ namespace ConsoleApplication3
             Q[head] = 0;
             return 1;
         }
-        int printData() {
+        public void printData(){
             if (head == tail)
             {
-                Console.WriteLine("errer; underflow.");
-                return 0;
+                Console.WriteLine("공백 큐입니다.");
+                //return 0;
             }
             foreach(int x in Q) {
-
+                Console.Write(x + "\t");
             }
+            Console.WriteLine();
+            //return 1;
         }
 
     }
