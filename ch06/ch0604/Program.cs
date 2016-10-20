95a57b11ae387dbbbaad9fdd6c9e85fb1a9ee6f0
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-10-20 16:21:53 +0900

    ch0604 overloading practice

---
+++ ch06/ch0604/Program.cs
@@ -0,0 +1,24 @@
+﻿//20161020 saori
+//p.312 ch6 04
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch0604
+{
+    class Program
+    {
+        static int Test(int A)  {return 10; }
+        static int Test(long A) { return 20;}
+        static int Test(float A)  { return 30; }
+        static int Test(double A) {return 40; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Test(52273));
+            Console.WriteLine(Test(52.273));
+        }
+    }
+}
