6456d204c82905fb43078470fd78ff23d8d59b8d
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-10-20 17:32:14 +0900

    constructor(생성자)\ n    same name with Class, no returntype, always public)-instance변수 초기화 위해서 있다   \n 생성자 만들 때(overloading) 최소 3가지 overloading한다(parameter의 1.아무것도 없는 것(상속떔),2.inctance변수 다들어가는것, 3.필수항목)

---
+++ ch06/Methods/Constructors.cs
@@ -0,0 +1,89 @@
+﻿//20161020 saori
+//ch06 p.270 Constructor (same name with Class, no returntype, always public)-instance변수 초기화 위해서 있다
+// instance(local)변수와 parameter는 이름을 같이 한다
+//constructor overloading
+
+// Class A ->변수a 1개 ->생성자2개;(),(a)
+//                 2개 ->생성자5개:(),(a),(b),(a,b),(b,a)
+//                 3개 ->(),(a),(b),(c),(a,b),(b,a),(a,c),(c,a),(b,c),(b,c),(a,b,c)...
+//                 n개 -> 1+n*n
+
+//생성자 만들 때(overloading) 최소 3가지 overloading한다(parameter의 1.아무것도 없는 것(상속떔),2.inctance변수 다들어가는것, 3.필수항목)
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Product
+    {
+        public static int counter = 0;
+        public int id;
+        public string name;
+        public int price;
+
+        public Product()             //constructor overloading; 매개변수 아무것도 없는 것
+        {         
+            Console.WriteLine("Product() constructor가 호출이 되었습니다.");
+        }           
+        public Product(string name,int price)   //constructor overloading; 다들어가는 것
+        {
+            Console.WriteLine("public Product(string name,int price) constructor가 호출이 되었습니다.");
+            Product.counter++;
+            this.id = Product.counter;
+            this.name = name;
+            this.price = price;
+        }
+        public Product(string name)         //constructor overloading; 필수 항복이 들어가는것
+        {  
+            Console.WriteLine(" public Product(string name)   constructor가 호출이 되었습니다.");
+            Product.counter++;
+            this.id = Product.counter;
+            this.name = name;
+            this.price = 0;
+        }
+
+        public string ToString() {
+            return id + "\t" + name + "\t" + price;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product(); //default 생성자
+            Product.counter++;
+            p1.id = Product.counter;
+            p1.name = "양파";
+            p1.price = 500;
+            Console.WriteLine(p1.id + "\t" + p1.name + "\t" + p1.price);
+
+            Product.counter++;
+            Product p2 = new Product()
+            {
+                id = Product.counter,
+                name = "대파",
+                price = 300
+            };
+            Console.WriteLine(p2.ToString());
+
+            Product p3 = new Product("파", 200);
+            Console.WriteLine(p3.ToString());
+
+            Product p4 = new Product("피망");
+            Console.WriteLine(p4.ToString());
+
+            Product p5 = new Product("감자");
+            p5.price = 2000;
+            Console.WriteLine(p5.ToString());
+
+            Product p6 = new Product("고구마");
+            p6.price = 3000;
+            Console.WriteLine(p6.ToString());
+        }
+    }
+}
