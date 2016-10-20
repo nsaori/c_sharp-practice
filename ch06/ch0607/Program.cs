0ff6b8d6d37d716f7f9c5e973ccb5b75e155cea7
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-10-20 18:04:36 +0900

    ch0607 연습문제 생성자,소멸자,상속

---
+++ ch06/ch0607/Program.cs
@@ -0,0 +1,37 @@
+﻿//20161020 ch0607 constructor
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch0607
+{
+    class A
+    {
+        public A() {
+            Console.WriteLine("A의 생성자");
+        }
+        ~A() {
+            Console.WriteLine("A의 소멸자");
+        }
+    }
+    class B : A
+    {
+        public B()
+        {
+            Console.WriteLine("B의 생성자");
+        }
+        ~B()
+        {
+            Console.WriteLine("B의 소멸자");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new B();
+        }
+    }
+}
