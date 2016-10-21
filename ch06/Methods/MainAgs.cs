70a632a4d7ae2e32af687b75e895e749153e9824
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-10-21 15:55:03 +0900

    MainArgs

---
+++ ch06/Methods/MainAgs.cs
@@ -0,0 +1,22 @@
+﻿//20161021 saori
+//ch06 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("args[" +i+ "]=" + args[i]);
+            }
+        }
+    }
+}
