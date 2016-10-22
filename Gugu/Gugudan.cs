3a6d71e061ab2bb9c182fadaa33cbf56f3897d43
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-10-22 13:02:58 +0900

    gugu-propaty,incapshlation

---
+++ Gugu/Gugudan.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gugu
+{
+    class Gugudan
+    {
+        int n;
+        public int N
+        {
+            get { return n; }
+            set { CheackNum(value); }
+        }
+        private void CheackNum(int n)
+        {
+            if (2 <= n&& n <= 9)
+            {
+                this.n = n;
+            }
+            else {
+                Console.WriteLine("2~9를 입력해주세요.");
+                Environment.Exit(0);
+            }
+        }
+
+        
+        public Gugudan(int n) {
+            N = n;
+        }
+        public Gugudan()
+        {
+            N = 2;
+        }
+
+        public void PrintAll()
+        {
+
+            for (int i = 1; i <= 9; i++) 
+            {
+                for (int j = 2; j <= N; j++)
+                {
+                    Console.Write(j+"x" +i +"="+ i*j +"\t");
+                 }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Gugu/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gugu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("단? ");
+            int input = int.Parse(Console.ReadLine());
+            Gugudan g = new Gugudan(input);
+            //출력
+            g.PrintAll();
+        }
+    }
+}
