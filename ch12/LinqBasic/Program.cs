17d694c4fa0515609a9be636b52410e6b313220a
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-10-28 16:44:28 +0900

    Linq

--- ch12/LinqBasic/Program.cs
+++ ch12/LinqBasic/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//p.509-514 Linq
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,15 +13,20 @@ namespace LinqBasic
         static void Main(string[] args)
         {
             List<int> input = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            /*
             List<int> output = new List<int>();
-
             foreach (int item in input)
             {
                 if (item % 2 == 0) {
                     output.Add(item);
                 }
-            }
-            foreach (int item in output)
+            }*/
+            //sql랑 분법이 똑같대
+            var output = from item in input
+                         where item % 2 == 0
+                         select item;
+           
+                foreach (int item in output)
             {
                 Console.WriteLine(item);
             }
