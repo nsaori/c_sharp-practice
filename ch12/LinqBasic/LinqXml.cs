d4430dea3cee5a993c2115e4f782567880939dd7
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-10-28 17:48:40 +0900

    Linq Xml

---
+++ ch12/LinqBasic/LinqXml.cs
@@ -0,0 +1,21 @@
+﻿//p.520
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DElegateRand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=4420033000";
+            XElement xElement = XElement.Load(url);
+            Console.WriteLine(xElement);
+        }
+    }
+}
