b0262f1e6c25272baa4e0ac411affcbccea848c5
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-10-28 18:19:32 +0900

    Linq XMl p.523

--- ch12/LinqBasic/LinqXml.cs
+++ ch12/LinqBasic/LinqXml.cs
@@ -15,39 +15,20 @@ namespace DElegateRand
         {
             string url = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=4420033000";
             XElement xElement = XElement.Load(url);
-            Console.WriteLine(xElement);
-            Console.WriteLine();
+            //Console.WriteLine(xElement);
+            //Console.WriteLine();
 
             var xmlQuery = from item in xElement.Descendants("data")
-                               //select item;
                            select new {
                                Hour = item.Element("hour").Value,
                                Day = item.Element("day").Value,
                                Temp = item.Element("temp").Value,
-                               WdKor = item.Element("wdkor").Value,
+                               WdKor = item.Element("wdKor").Value,
                                WfKor = item.Element("wfKor").Value,
                                Tmn = item.Element("tmn").Value,
                                Tmx = item.Element("tmx").Value
                            };
 
-            /*
-            foreach (var item in xmlQuery)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine();*/
-            /*
-            foreach (var item in xmlQuery)
-            {
-                Console.Write(item.Element("hour").Value+"\t");
-                Console.Write(item.Element("day").Value + "\t");
-                Console.Write(item.Element("temp").Value + "\t");
-                Console.Write(item.Element("wdkor").Value + "\t");
-                Console.Write(item.Element("wfKor").Value + "\t");
-                Console.Write(item.Element("tmn").Value + "\t");
-                Console.Write(item.Element("tmx").Value + "\t");
-                Console.WriteLine();
-            }*/
             foreach (var item in xmlQuery)
             {
                 Console.Write(item.Hour + "\t");
