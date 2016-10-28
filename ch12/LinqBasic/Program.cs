aec96c24fa0c2fdb683a6689edd0c6b4d824af13
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-10-28 16:39:13 +0900

    Linq before linq

---
+++ ch12/LinqBasic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> output = new List<int>();
+
+            foreach (int item in input)
+            {
+                if (item % 2 == 0) {
+                    output.Add(item);
+                }
+            }
+            foreach (int item in output)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
